71cafda1770ba4afef50a71e097ccd9970aad8f7
Roel Canicula <roelmc@info.com.ph>
2005-08-03 22:49:48 +0000

    Record scholarship grants

---
+++ sql/postgresql/scholarship-fund-create.sql
@@ -0,0 +1,17 @@
+-- 
+-- packages/scholarship-fund/sql/postgresql/scholarship-fund-create.sql
+-- 
+-- @author Roel Canicula (roelmc@pldtdsl.net)
+-- @creation-date 2005-08-03
+-- @arch-tag: 904b71ab-89cf-4c36-af73-7fc85414a6bc
+-- @cvs-id $Id$
+--
+
+create table scholarship_fund_grants (
+	grant_id		serial primary key,
+	fund_id			integer references scholarship_fund on delete cascade not null,
+	user_id			integer references users on delete cascade not null,
+	grant_date		timestamp default current_timestamp not null,
+	gift_certificate_id 	integer references ec_gift_certificates not null,
+	grant_amount		float not null
+);
